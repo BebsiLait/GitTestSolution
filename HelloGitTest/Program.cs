a8a26cce26c2e600a45a28dc8b1d1744d3a31518
azoTahaAli <taha.ali@i-azo.ch>
2022-02-01 11:00:38 +0100

    neue Ausgabe hinzugefügt

--- HelloGitTest/Program.cs
+++ HelloGitTest/Program.cs
@@ -7,6 +7,7 @@ namespace HelloGitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("BebsiLait");
         }
     }
 }
